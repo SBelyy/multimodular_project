c065b6b721961f3091b5a7295244a992242f9f2a
Alex Zhydok <alex.zhydok@mail.ru>
2021-03-27 16:14:31 +0300

    Added binary search

--- modulea/src/main/java/Runner.java
+++ modulea/src/main/java/Runner.java
@@ -1,7 +1,32 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Runner {
 
+    private static final int SIZE_ARRAY = 1000;
+    private static final Logger logger = LoggerFactory.getLogger(Runner.class);
+
     public static void main(String[] args) {
-        
+        int[] array = getFilledArray(SIZE_ARRAY);
+        int elementToFind = 3;
+
+        logger.info("Start binary search. Initial data: Start index - 0, " +
+                "End Index - " + SIZE_ARRAY + ", Element to find - " + elementToFind);
+        int numberElement = Algorithm.binarySearch(array, 0, SIZE_ARRAY, elementToFind);
+        if (numberElement < 0) {
+            logger.error("Array element not found");
+        } else {
+            logger.info("Binary search finish. Element index - " + numberElement);
+        }
+
+    }
+
+    private static int[] getFilledArray(int length) {
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = i;
+        }
+        return array;
     }
 
 }
--- moduleb/src/main/java/Algorithm.java
+++ moduleb/src/main/java/Algorithm.java
@@ -1,7 +1,27 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Algorithm {
 
-    public static int binarySearch(int[] array) {
-        return 0;
+    private static final Logger logger = LoggerFactory.getLogger(Algorithm.class);
+
+    public static int binarySearch(int[] array, int startIndex, int endIndex, int elementToFind) {
+        if (endIndex >= startIndex) {
+            int middleIndex = startIndex + (endIndex - startIndex) / 2;
+
+            logger.info("Search process. " + "Data: Start Index - " + startIndex + ", End index - " +
+                    endIndex + ", Middle Index - " + middleIndex + ".");
+            if (array[middleIndex] == elementToFind) {
+                return middleIndex;
+            }
+
+            if (array[middleIndex] > elementToFind) {
+                return binarySearch(array, startIndex, middleIndex - 1, elementToFind);
+            } else {
+                return binarySearch(array, middleIndex + 1, endIndex, elementToFind);
+            }
+        }
+        return -1;
     }
 
 }
